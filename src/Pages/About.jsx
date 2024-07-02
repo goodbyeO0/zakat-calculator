b9c735aabe81103ea38ef328a49d49e6e3675433
goodbyeO0 <zikrizhan@gmail.com>
2024-07-02 17:47:16 +0800

    test git 2

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function About() {
     return (
         <>
-            <div className=" text-6xl">About</div>
+            <div className=" text-6xl">Abouttt</div>
             <Link to="/">Home</Link>
         </>
     )
